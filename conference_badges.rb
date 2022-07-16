ddc33d9298588902a179e51b6d2c94c56c121287
Elizabeth Klenczar <eklenczar@gmail.com>
2022-07-16 15:31:07 -0400

    completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+    name_array.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms(name_array)
+    name_array.map.with_index(1) do |name, room_num|
+        "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def print_array(array)
+    array.each { |element| puts element}
+end
+
+def printer(name_array)
+    print_array(batch_badge_creator(name_array))
+    print_array(assign_rooms(name_array))
+end
